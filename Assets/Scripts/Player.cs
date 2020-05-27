f589b3974c01eaea7752f203fe093f21cff724fb
Jeff Anderson <captainqwyx@gmail.com>
2020-05-26 21:04:26 -0500

    clamp player movement to viewport

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 6f;
 
+    // configuration
+    float xMin = 0f;
+    float xMax = 0f;
+    float yMin = 0f;
+    float yMax = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupBoundaries();
     }
 
     // Update is called once per frame
@@ -22,8 +28,18 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetupBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 }
