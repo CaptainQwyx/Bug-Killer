7a745f2f887feaddfb38e198fd54eece9f1e8b47
Jeff Anderson <captainqwyx@gmail.com>
2020-05-30 17:47:39 -0500

    player death to game over

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] float delayInSeconds = 2f;
 
     public void LoadStartMenu()
     {
@@ -18,6 +20,12 @@ public class Level : MonoBehaviour
 
     public void LoadGameOver()
     {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
         SceneManager.LoadScene("Game Over");
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -63,6 +63,7 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
+        FindObjectOfType<Level>().LoadGameOver();
         Destroy(gameObject);
         AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathVolume);
     }
